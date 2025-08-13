bf2204535f8a14e60bd6c73039c69db2dec1ee53
GustavoZP <gustavozarthperotto@gmail.com>
2025-08-13 09:47:13 -0300

    Primeiro commit

---
+++ src/main/java/com/themyra/validacao/themyra1/Themyra1Application.java
@@ -0,0 +1,13 @@
+package com.themyra.validacao.themyra1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Themyra1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Themyra1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/themyra/validacao/themyra1/Themyra1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themyra.validacao.themyra1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Themyra1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
